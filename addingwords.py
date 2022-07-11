f0bd74affacbd690b65df343f183baf02394f07b
Jakob <jakob.m@dsen.tv>
2022-07-11 21:52:33 +0200

    update some solutions

--- addingwords.py
+++ addingwords.py
@@ -13,23 +13,18 @@ for line in stdin:
         symbols = ops[1:]
         value = 0
         equation = ""
-        # print('MEM', mem)
 
         for s in symbols:
-            # print('S', s)
             if s == '=':
                 continue
             if s == '+' or s == '-':
                 equation += " "+ s +" "
             else:
                 if s not in mem:
-                    # print(s, 'not in MEM')
                     print(' '.join(ops[1:]), 'unknown')
                     break
                 equation += str(mem[s])
         else:
-            # print('A', symbols)
-            # print('B', equation)
             value = eval(equation)
 
             for k, v in mem.items():
@@ -38,25 +33,6 @@ for line in stdin:
                     break
             else:
                 print(' '.join(ops[1:]), 'unknown')
-
-
-
-        # for idx, var in enumerate(ops[1:]):
-        #     print(idx, var)
-        #     if var in ['calc', '+', '-']:
-        #         continue
-        #     if var not in mem.keys():
-        #         print(' '.join(ops[1:]), 'unknown')
-        #         break
-        #     value = value + ( mem[var] if ops[idx-1] != '-' else -mem[var] )
-        #     print('VALUE', value)
-        # else:
-        #     for v, k in mem.items():
-        #         if v == value:
-        #             print(' '.join(ops[1:]), k)
-        #             break
-        #     else:
-        #         print(' '.join(ops[1:]), 'unknown')
         
     elif ops[0] == 'clear':
         mem = {}
