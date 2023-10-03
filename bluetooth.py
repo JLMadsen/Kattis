b8222e980d9b89e3623b6ac6759c31cec731636d
Jakob <jakob.m@dsen.tv>
2023-10-03 22:37:26 +0200

    Update bluetooth.py

--- bluetooth.py
+++ bluetooth.py
@@ -1,4 +1,4 @@
 import sys
 m,d=[[8,8],[8,8]],sys.stdin;next(d)
-for(a,b,_,c,_)in d:a,b=[a,b][::1-2*(l:=a not in'+-')];m[a=='+'][l]-=1*c=='m'or 9
+for(i)in d:m['+'in i][i[0].isdigit()]-=9**(i[3]=='b')
 [a,b],[c,d]=m;print([a>0<c,d>0<b,1].index(1))
