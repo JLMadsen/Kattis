2298aa1d73441fd162f5c8448bc30a42114bccda
Jakob <jakob.m@dsen.tv>
2023-10-03 23:19:49 +0200

    Update bluetooth.py

--- bluetooth.py
+++ bluetooth.py
@@ -1,4 +1,4 @@
 import sys
-m,d=[[8,8],[8,8]],[*sys.stdin]
-for(i)in d[1:]:m['+'in i][i[1]in'+-']-=9**('b'in i)
-[a,b],[c,d]=m;print([a>0<c,d>0<b,1].index(1))
+m,d=[8]*4,[*sys.stdin]
+for(i)in d[1:]:m[2*('+'in i)+(i[1]in'+-')]-=9**('b'in i)
+a,b,c,d=m;print([a>0<c,d>0<b,1].index(1))
