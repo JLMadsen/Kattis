908813817083ecd0e51195ad3f40f53fc2e80c24
Jakob <jakob.m@dsen.tv>
2023-10-04 19:17:17 +0200

    Update okviri.py

--- okviri.py
+++ okviri.py
@@ -1,12 +1,6 @@
-t,l,e = input(),['' for _  in range(5)], enumerate
-a=lambda h: [l[j]+c for j,c in e(h)]
-y=0
-for i, d in e(t,1):
-    p='*' if (w:=i>0 and i%3==0) else '#'
-    if not y:
-        l = [c[:-1] for c in l]
-        l = a(f'..{p}..')
-    for s in [f'.{p}.{p}.',f'{p}.{d}.{p}',f'.{p}.{p}.',f'..{p}..']:
-        l = a(s)
-    y=w
+for i,d in e(t,1):
+    p='*'if(w:=i>0 and i%3==0)else'#';s,m=f'..{p}..',f'.{p}.{p}.'
+    if not y:l=[c[:-1]for c in l];l=a(s)
+    for s in[m,f'{p}.{d}.{p}',m,s]:l=a(s);y=w
 print('\n'.join(l))
+
