dd7b6561d6cb1e6dda5cae35233d2a8df7c1f37d
Jakob <jakob.m@dsen.tv>
2023-10-04 19:17:27 +0200

    Update okviri.py

--- okviri.py
+++ okviri.py
@@ -1,3 +1,4 @@
+t,l,e=input(),['']*5,enumerate;a=lambda h:[l[j]+c for j,c in e(h)];y=0
 for i,d in e(t,1):
     p='*'if(w:=i>0 and i%3==0)else'#';s,m=f'..{p}..',f'.{p}.{p}.'
     if not y:l=[c[:-1]for c in l];l=a(s)
